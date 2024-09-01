2751ab8e138ac7a4530c190a31f422ff5af21f81
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-09-01 13:30:18 +0100

    Add StdInc

--- EagleAgentPE/Main.h
+++ EagleAgentPE/Main.h
@@ -1,16 +1,5 @@
 #pragma once
-#include <d3d11.h>
-#include <windows.h>
-#include <dwmapi.h>
-#include <string>
-#include <D3DX11tex.h>
-#pragma comment(lib, "D3DX11.lib")
-
-#include "imgui.h"
-#include "imgui_internal.h"
-#include "imgui_impl_win32.h"
-#include "imgui_impl_dx11.h"
-
+#include "StdInc.h"
 
 #define size_of IM_ARRAYSIZE
 
