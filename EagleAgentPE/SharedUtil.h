008512f8ef5d1c43bef127e5c58b12adb39f2b2b
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-09-07 12:13:47 +0100

    Update utils

---
+++ EagleAgentPE/SharedUtil.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <Windows.h>
+
+namespace SharedUtil
+{
+    bool TerminateProcess(DWORD dwPID);
+    int GetProcessID(const char* szProcessName);
+    bool FindStringIC(const std::string& strHaystack, const std::string& strNeedle);
+    int GenerateRandomNumber(int min, int max);
+}
