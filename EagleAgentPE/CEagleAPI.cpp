4d6bbbdd606661bd2849dfb97b62c6abb64d6671
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-09-07 12:20:06 +0100

    Add Eagke API wrapper

---
+++ EagleAgentPE/CEagleAPI.cpp
@@ -0,0 +1,65 @@
+#include "CEagleAPI.h"
+#include "CEagleCore.h"
+
+CEagleAPI* g_pEagleAPI = new CEagleAPI();
+
+CEagleAPI::CEagleAPI()
+{
+}
+
+CEagleAPI::~CEagleAPI()
+{
+}
+
+bool CEagleAPI::DownloadAgentPEB()
+{
+    std::string peb_buffer = PostRequest("http://127.0.0.1:8000/resources/agentpeb");
+    printf("agent peb buffer size: %d\n", peb_buffer.size());
+
+    return true;
+}
+
+std::string CEagleAPI::PostRequest(const char* szURL, jsoncons::json Data, bool bEncryptRequestBody, bool bDecryptRespnseBody)
+{
+    CURL* curl;
+    CURLcode response_code;
+    std::string response_buffer;
+
+    curl = curl_easy_init();
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, szURL);
+
+
+        auto request_body_buffer = bEncryptRequestBody ? g_pEagleCore->Encrypt((BYTE*)Data.to_string().c_str()) : (BYTE*)Data.to_string().c_str();
+
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request_body_buffer);
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_buffer);
+
+        response_code = curl_easy_perform(curl);
+        if (response_code != CURLE_OK)
+        {
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(response_code) << std::endl;
+        }
+    }
+
+    if (bDecryptRespnseBody)
+    {
+        printf("Encrypted response size: %d\n", response_buffer.size());
+        std::string out = g_pEagleCore->Decrypt((BYTE*)response_buffer.c_str());
+        printf("%c%c\n", out.at(0), out.at(1));
+
+        return out;
+    }
+
+    return response_buffer;
+}
+
+size_t CEagleAPI::WriteCallback(void* contents, size_t size, size_t nmemb, std::string* response)
+{
+    size_t total_size = size * nmemb;
+    response->append((char*)contents, total_size);
+    return total_size;
+}
---
+++ EagleAgentPE/CEagleAPI.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "StdInc.h"
+#include "jsoncons/json.hpp"
+
+class CEagleAPI
+{
+public:
+    CEagleAPI();
+    ~CEagleAPI();
+
+    bool DownloadAgentPEB();
+
+private:
+    std::string PostRequest(const char* szURL, jsoncons::json Data = jsoncons::json(), bool bEncryptRequestBody = true, bool bDecryptRespnseBody = true);
+    static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* response);
+};
+
+extern CEagleAPI* g_pEagleAPI;
