c19bd356a9cda5bfdc048219b44b4fe62514cd92
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-09-07 22:39:39 +0100

    Update dllmain.cpp

--- EagleAgentPEB/dllmain.cpp
+++ EagleAgentPEB/dllmain.cpp
@@ -1,4 +1,8 @@
 #include <Windows.h>
+void EntryPoint(void*)
+{
+    MessageBox(0, "Injected", 0, 0);
+}
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -7,8 +11,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 {
     switch (ul_reason_for_call)
     {
-    case DLL_PROCESS_ATTACH:
-        MessageBox(0, "Injected", 0, 0);
+    case DLL_PROCESS_ATTACH:        
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)EntryPoint, 0, 0, 0);
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
