eaa5ed37b491285c7be2396d12c35c9c32c28d36
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-09-08 17:31:37 +0100

    Update CEagleNetwork class
    Add WaitResponse()
    Add some private variables
    Implement OnReceivePacket()
    Implement SendPacket()

--- EagleAgentPEB/CEagleNetwork.cpp
+++ EagleAgentPEB/CEagleNetwork.cpp
@@ -1,4 +1,6 @@
 #include "CEagleNetwork.h"
+#include <condition_variable>
+#include <future>
 
 CEagleNetwork* g_pEagleNetwork = new CEagleNetwork();
 
@@ -15,17 +17,49 @@ bool CEagleNetwork::Connect()
 {
     if (!ix::initNetSystem())
         return false;
-    
+
     m_pWebSocket->setUrl("ws://127.0.0.1:8000/c/eaglescanner/");
 
     m_pWebSocket->setOnMessageCallback(std::bind(&CEagleNetwork::OnReceivePacket, this, std::placeholders::_1));
 
     ix::WebSocketInitResult result = m_pWebSocket->connect(32);
+    m_pWebSocket->start();
 
     return result.success;
 }
 
+void CEagleNetwork::SendPacket(eEaglePacketID PacketID, jsoncons::json Data)
+{
+    // Allocate new json object
+    jsoncons::json PacketJson = jsoncons::json::object();
+
+    // Set The Packet Type
+    PacketJson["type"] = (unsigned short)PacketID;
+
+    // Fill the new json with the data items
+    for (const auto& Iter : Data.object_range())
+        PacketJson[Iter.key()] = Iter.value();
+
+    // Send the packet to eagle master server
+    m_pWebSocket->send(PacketJson.to_string());
+}
+
+jsoncons::json CEagleNetwork::WaitReponse(eEaglePacketID PacketID)
+{
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_condition.wait(lock, [&] { return m_UnhandledPackets.find(PacketID) != m_UnhandledPackets.end(); });
+    jsoncons::json Response = m_UnhandledPackets[PacketID];
+    m_UnhandledPackets.erase(PacketID);
+    return Response;
+}
+
 void CEagleNetwork::OnReceivePacket(const ix::WebSocketMessagePtr& Message)
 {
-
+    if (Message->type == ix::WebSocketMessageType::Message)
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        jsoncons::json              json = jsoncons::json::parse(Message->str);
+        m_UnhandledPackets.insert_or_assign((eEaglePacketID)json["type"].as_int(), json);
+        m_condition.notify_one();
+    }
 }
--- EagleAgentPEB/CEagleNetwork.h
+++ EagleAgentPEB/CEagleNetwork.h
@@ -4,6 +4,8 @@
 #include <ixwebsocket/IXNetSystem.h>
 #include <ixwebsocket/IXWebSocket.h>
 #include <ixwebsocket/IXUserAgent.h>
+#include <mutex>
+#include <map>
 
 class CEagleNetwork
 {
@@ -13,11 +15,15 @@ public:
 
     bool Connect();
     void SendPacket(eEaglePacketID PacketID, jsoncons::json Data = jsoncons::json());
+    jsoncons::json WaitReponse(eEaglePacketID PacketID);
 
     void OnReceivePacket(const ix::WebSocketMessagePtr& Message);
 
 private:
     ix::WebSocket* m_pWebSocket;
+    std::mutex     m_mutex;
+    std::condition_variable m_condition;
+    std::map<eEaglePacketID, jsoncons::json> m_UnhandledPackets;
 };
 
 extern CEagleNetwork* g_pEagleNetwork;
