f7570a789d39e9dc8d8ec47469d975dfa1a1498d
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-09-08 17:04:30 +0100

    Create StdInc.h

---
+++ EagleAgentPEB/StdInc.h
@@ -0,0 +1,12 @@
+#pragma once
+// Third Parties
+// Jsoncons
+#include "jsoncons/json.hpp"
+
+// IXWebSocket
+#include <ixwebsocket/IXNetSystem.h>
+#include <ixwebsocket/IXWebSocket.h>
+#include <ixwebsocket/IXUserAgent.h>
+
+#include "EaglePacketID.h"
+#include "CEagleNetwork.h"
