b42883599d1202954854967f4ddba8c5a77f69fe
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-10-05 20:44:02 +0100

    Update CEagleAPI.cpp

--- EagleAgentPE/CEagleAPI.cpp
+++ EagleAgentPE/CEagleAPI.cpp
@@ -19,7 +19,7 @@ jsoncons::json CEagleAPI::GetStatus()
         jsoncons::json JsonResponse = jsoncons::json::object();
         JsonResponse["alive"] = false;
         JsonResponse["title"] = "Connection Error";
-        JsonResponse["message"] = "Failed to connect to Eagle Master Server";
+        JsonResponse["message"] = "Failed to connect to SafeGuard Master Server";
 
         return JsonResponse;
     }
