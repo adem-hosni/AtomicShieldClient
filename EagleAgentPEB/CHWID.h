f234a124514dbe06e3c696e989186cafefa33866
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-10-26 19:55:00 +0100

    Update CHWID.h

--- EagleAgentPEB/CHWID.h
+++ EagleAgentPEB/CHWID.h
@@ -7,12 +7,11 @@ public:
     CHWID();
     ~CHWID();
 
-    std::string        GetMTASerial();
-    std::string        GetWindowsUsername();
-    std::string        GetMotherBoardSerial();
+    std::string    GetMTASerial();
+    std::string    GetWindowsUsername();
+    std::string    GetMotherBoardSerial();
     jsoncons::json GetDisksSerialNumber();
-    std::string        GetCPUsSerials();
-    std::string        GetBIOSVersion();
+    std::string    GetCPUsSerials();
+    std::string    GetBIOSVersion();
 };
-
 extern CHWID* g_pHWID;
