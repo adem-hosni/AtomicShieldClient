45fc877c8e9adc89079fdf8f94e5c04c15e1368b
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-12-05 21:51:28 +0100

    Fivem Engine: Update hwid class

--- FivemEngine/CHWID.cpp
+++ FivemEngine/CHWID.cpp
@@ -17,96 +17,14 @@ CHWID::~CHWID()
 
 jsoncons::json CHWID::GetExtraData()
 {
-    jsoncons::json    json = jsoncons::json();
-    std::string       strMTASerial = "<unkown>";
-    CLiteRegeditEasy* pRegistry =
-        new CLiteRegeditEasy(HKEY_LOCAL_MACHINE, ("SOFTWARE\\WOW6432Node\\Multi Theft Auto: San Andreas All\\1.6\\Settings\\general"));
-
-    if (pRegistry)
-    {
-        strMTASerial = pRegistry->ReadString(("serial"));
-    }
-    json["mta_serial"] = strMTASerial;
+    jsoncons::json    json = jsoncons::json::object();
+    
     return json;
 }
 
-std::string CHWID::GetMonitorSerial()
+jsoncons::json CHWID::GetMonitorSerial()
 {
-    HRESULT hres;
-
-    hres = CoInitializeEx(0, COINIT_MULTITHREADED);
-    if (FAILED(hres))
-        return "<unknown>";
-
-    // Obtain the WMI locator
-    IWbemLocator* pLoc = NULL;
-    hres = CoCreateInstance(CLSID_WbemLocator, 0, CLSCTX_INPROC_SERVER, IID_IWbemLocator, (LPVOID*)&pLoc);
-    if (FAILED(hres))
-    {
-        CoUninitialize();
-        return "<unknown>";
-    }
-
-    // Connect to WMI
-    IWbemServices* pSvc = NULL;
-    hres = pLoc->ConnectServer(_bstr_t(L"ROOT\\CIMV2"), NULL, NULL, 0, NULL, 0, 0, &pSvc);
-    if (FAILED(hres))
-    {
-        pLoc->Release();
-        CoUninitialize();
-        return "<unknown>";
-    }
-
-    // Set security levels on the proxy
-    hres = CoSetProxyBlanket(pSvc, RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, NULL, RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE);
-    if (FAILED(hres))
-    {
-        pSvc->Release();
-        pLoc->Release();
-        CoUninitialize();
-        return "<unknown>";
-    }
-
-    // Query the monitor information
-    IEnumWbemClassObject* pEnumerator = NULL;
-    hres =
-        pSvc->ExecQuery(bstr_t("WQL"), bstr_t("SELECT * FROM Win32_DesktopMonitor"), WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, NULL, &pEnumerator);
-    if (FAILED(hres))
-    {
-        pSvc->Release();
-        pLoc->Release();
-        CoUninitialize();
-        return "<unknown>";
-    }
-
-    // Retrieve the data from the query
-    IWbemClassObject* pclsObj = NULL;
-    ULONG             uReturn = 0;
-    std::string       strMonitorSerial = "<unknown>";
-
-    while (pEnumerator)
-    {
-        hres = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
-        if (0 == uReturn)
-            break;
-
-        VARIANT vtProp;
-        // Get the serial number (PNPDeviceID) property
-        hres = pclsObj->Get(L"PNPDeviceID", 0, &vtProp, 0, 0);
-        if (SUCCEEDED(hres))
-        {
-            strMonitorSerial = static_cast<const char*>(_bstr_t(vtProp.bstrVal));
-        }
-
-        VariantClear(&vtProp);
-        pclsObj->Release();
-    }
-
-    // Cleanup
-    pSvc->Release();
-    pLoc->Release();
-    CoUninitialize();
-    return strMonitorSerial;
+    return jsoncons::json::array();
 }
 
 std::string CHWID::GetWindowsUsername()
@@ -521,8 +439,8 @@ std::string CHWID::GetPNPDeviceID()
         if (SUCCEEDED(hr))
         {
             strPNPDeviceID = static_cast<const char*>(_bstr_t(vtProp.bstrVal));
-            VariantClear(&vtProp);
         }
+        VariantClear(&vtProp);
 
         pclsObj->Release();
     }
