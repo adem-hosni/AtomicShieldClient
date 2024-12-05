f8be20dcb60c1c72108d5c0d002b603644eb41b4
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-12-05 21:51:46 +0100

    Update Common.h

--- Shared/Common.h
+++ Shared/Common.h
@@ -3,5 +3,8 @@
 #define PROJECT_NAME "SafeGuard"
 
 // API Commons
-#define API_BASE_URL "http://51.195.45.15"
-#define WEBSOCKET_BASE_URL "ws://51.195.45.15:8000"
+//#define API_BASE_URL       "http://51.195.45.15"
+//#define WEBSOCKET_BASE_URL "ws://51.195.45.15:8000"
+
+#define API_BASE_URL       "http://127.0.0.1:8000"
+#define WEBSOCKET_BASE_URL "ws://127.0.0.1:8000"
