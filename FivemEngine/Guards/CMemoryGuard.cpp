026d3d42ad5c0c8885b568d06aa6037e2a9a639e
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-12-19 22:17:47 +0100

    Update memory guard pulse

--- FivemEngine/Guards/CMemoryGuard.cpp
+++ FivemEngine/Guards/CMemoryGuard.cpp
@@ -1,10 +1,13 @@
 #include "StdInc.h"
 
+#define START_ADDRESS (PVOID)0x00000000010000
+#define END_ADDRESS   (0x00007FF8F2580000 - 0x00000000010000)
+
 CMemoryGuard::CMemoryGuard()
 {
     m_mbi = {0};
     m_systemInfo = {0};
-    m_dwCurrentAddress = NULL;
+    m_dwCurrentAddress = (DWORD)START_ADDRESS;
     m_dwMaxAddress = NULL;
 }
 
@@ -16,52 +19,44 @@ CMemoryGuard::~CMemoryGuard()
 
 void CMemoryGuard::DoPulse()
 {
-    DWORD64 dwEnd = m_dwCurrentAddress + m_dwMaxAddress;
-    DWORD   mask = (PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_READ);
-
-    //NT_SUCCESS(NtQueryVirtualMemory(GetCurrentProcess(), reinterpret_cast<PVOID>(m_dwCurrentAddress), MemoryBasicInformation, &m_mbi, sizeof(m_mbi), 0))
-    SharedUtil::AddDebugLog("Begin Memory Guard Scan");
-    while (m_dwCurrentAddress < dwEnd && VirtualQuery((void*)m_dwCurrentAddress, &m_mbi, sizeof(m_mbi)))
+    while (true)
     {
-        if ((m_mbi.State != MEM_FREE && m_mbi.State != MEM_RELEASE) && m_mbi.Protect & mask)
-        {
-            BYTE    completeSequence = 0;
-            DWORD64 dwFoundIAT = 0x0;
-            for (DWORD64 z = m_dwCurrentAddress; z < m_dwCurrentAddress + m_mbi.RegionSize; z++)
-            {
-                for (DWORD x = 0; x < (8 * 6); x += 0x6)
-                {
-                    if ((x + z) < m_dwCurrentAddress + m_mbi.RegionSize && (x + z + 0x1) < m_dwCurrentAddress + m_mbi.RegionSize)
-                    {
-                        if ((*(BYTE*)(z + x) == 0xFF && *(BYTE*)(x + z + 0x1) == 0x25))
-                        {
-                            dwFoundIAT = (x + z);
-                            completeSequence++;
-                        }
-                        else
-                        {
-                            completeSequence = 0;
-                        }
-                    }
-                }
+        DWORD                    mask = (PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_READ);
+        MEMORY_BASIC_INFORMATION info;
+        const void*              pCurrentAddress = START_ADDRESS;
+        const void*              end = (const void*)((const char*)pCurrentAddress + END_ADDRESS);
 
-                if (completeSequence >= 8)
+        while (pCurrentAddress < end && VirtualQuery(pCurrentAddress, &info, sizeof(info)) == sizeof(info))
+        {
+            if ((info.State != MEM_FREE && info.State != MEM_RELEASE) && info.Type & (MEM_IMAGE | MEM_PRIVATE) && info.Protect & mask)
+            {
+                if (!Utils::IsAddressInModuledRange((DWORD64)pCurrentAddress))
                 {
-                    SharedUtil::AddDebugLog("Completed sequence 0x%x", completeSequence);
-                    completeSequence = NULL;
-                    char szMappedName[256];
-                    memset(szMappedName, 0, sizeof(szMappedName));
-                    GetMappedFileName(g_pSafeAntiCheat->GetProcessHandle(), m_mbi.BaseAddress, szMappedName, sizeof(szMappedName));
-                    std::string strPossibleModuleName = Utils::ParseModuleNameFromPath(szMappedName);
-                    bool        bCloaked = false;
-                    if (!Utils::IsAddressInModuledRange((DWORD)m_mbi.BaseAddress))
+                    for (DWORD_PTR z = (DWORD_PTR)pCurrentAddress; z < ((DWORD_PTR)pCurrentAddress + info.RegionSize); z++)
                     {
-                        SharedUtil::AddDebugLog("Detected cloaked module at 0x%x (possible module name: %s)", strPossibleModuleName.c_str());
+                        bool bCompletedSequence = false;
+                        __try
+                        {
+                            for (DWORD x = 0; x < (10 * 6); x += 0x6)
+                                bCompletedSequence = *(byte*)(z + x) == 0xFF && *(byte*)(x + z + 0x1) == 0x25;
+                        }
+                        __except (EXCEPTION_EXECUTE_HANDLER)
+                        {
+                        };
+                        if (bCompletedSequence)
+                        {
+                            SMemoryDetectionReport Report;
+                            Report.AllocatedBase = info.AllocationBase;
+                            Report.AllocatedProtect = info.AllocationProtect;
+                            Report.RegionSize = info.RegionSize;
+
+                            g_pSafeAntiCheat->NotifyDetection(eDetectionType::UNRECOGNISED_IAT_FOUND, &Report);
+                            break;
+                        }
                     }
                 }
             }
+            pCurrentAddress = (const void*)((const char*)(info.BaseAddress) + info.RegionSize);
         }
-        m_dwCurrentAddress = (DWORD)m_mbi.BaseAddress + m_mbi.RegionSize;
     }
-    SharedUtil::AddDebugLog("End Memory Guard Scan");
 }
