1ba4d00059eea264cdd9f19f4527641e04dafe30
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-12-19 15:23:23 +0100

    Update CMemoryGuard class

--- FivemEngine/Guards/CMemoryGuard.h
+++ FivemEngine/Guards/CMemoryGuard.h
@@ -2,18 +2,11 @@
 #include "StdInc.h"
 #include "CGuardBase.h"
 
-class CMemoryGuard : public CGuardBase
+class CMemoryGuard final : public CGuardBase
 {
 public:
     CMemoryGuard();
     ~CMemoryGuard();
 
-    void Initialize() override;
     void DoPulse() override;
-
-private:
-    MEMORY_BASIC_INFORMATION m_mbi;
-    SYSTEM_INFO              m_systemInfo;
-    DWORD64                  m_dwMaxAddress;
-    DWORD64                  m_dwCurrentAddress;
 };
