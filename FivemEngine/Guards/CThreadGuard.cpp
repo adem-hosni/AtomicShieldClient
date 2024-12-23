20afc16a2d65c3cad5c294fbe1d8ae95c759a106
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-12-23 20:10:58 +0100

    Update CThreadGuard.cpp

--- FivemEngine/Guards/CThreadGuard.cpp
+++ FivemEngine/Guards/CThreadGuard.cpp
@@ -30,31 +30,34 @@ void CThreadGuard::DoPulse()
                 if (th32.th32OwnerProcessID == GetCurrentProcessId() && th32.th32ThreadID != GetCurrentThreadId())
                 {
                     HANDLE hThread = OpenThread(THREAD_ALL_ACCESS, FALSE, th32.th32ThreadID);
-                    if (hThread && !g_pSafeAntiCheat->IsAtomicThread(hThread))
+                    if (hThread)
                     {
-                        DWORD64 dwTempoaryBase = NULL;
-                        NtQueryInformationThread(hThread, (THREADINFOCLASS)9, &dwTempoaryBase, sizeof(DWORD64), NULL);
-                        DWORD64 dwModuleBase = Utils::IsAddressInModuledRange(dwTempoaryBase);
-                        if (dwModuleBase == -1)
+                        if (!g_pSafeAntiCheat->IsAtomicThread(hThread))
                         {
-                            iMaliciousThreadCount++;
-                            if (iMaliciousThreadCount > 1 || true)
+                            DWORD64 dwTempoaryBase = NULL;
+                            NtQueryInformationThread(hThread, (THREADINFOCLASS)9, &dwTempoaryBase, sizeof(DWORD64), NULL);
+                            DWORD64 dwModuleBase = Utils::IsAddressInModuledRange(dwTempoaryBase);
+                            if (dwModuleBase == -1)
                             {
-                                MEMORY_BASIC_INFORMATION mbi;
-                                SMemoryDetectionReport   Report;
+                                iMaliciousThreadCount++;
+                                if (iMaliciousThreadCount > 1)
+                                {
+                                    MEMORY_BASIC_INFORMATION mbi;
+                                    SMemoryDetectionReport   Report;
 
-                                VirtualQuery((LPCVOID)dwTempoaryBase, &mbi, sizeof(th32.dwSize));
+                                    VirtualQuery((LPCVOID)dwTempoaryBase, &mbi, sizeof(th32.dwSize));
 
-                                Report.AllocatedBase = mbi.AllocationBase;
-                                Report.AllocatedProtect = mbi.AllocationProtect;
-                                Report.RegionSize = mbi.RegionSize;
+                                    Report.AllocatedBase = mbi.AllocationBase;
+                                    Report.AllocatedProtect = mbi.AllocationProtect;
+                                    Report.RegionSize = mbi.RegionSize;
 
-                                SharedUtil::AddDebugLog("Detected Malicious Thread at 0x%x", dwTempoaryBase);
-                                g_pSafeAntiCheat->NotifyDetection(eDetectionType::UNAUTHORIZED_THREAD, &Report);
+                                    SharedUtil::AddDebugLog("Detected Malicious Thread at 0x%x", dwTempoaryBase);
+                                    g_pSafeAntiCheat->NotifyDetection(eDetectionType::UNAUTHORIZED_THREAD, &Report);
+                                }
                             }
                         }
+                        CloseHandle(hThread);
                     }
-                    CloseHandle(hThread);
                 }
             } while (Thread32Next(hSnapshot, &th32));
         }
