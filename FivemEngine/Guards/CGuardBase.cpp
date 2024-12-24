db2e3632b0aee3004375cdf04828e061f7c84fbc
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2024-12-24 16:41:02 +0100

    Update CGuardBase

--- FivemEngine/Guards/CGuardBase.cpp
+++ FivemEngine/Guards/CGuardBase.cpp
@@ -2,8 +2,7 @@
 
 void CGuardBase::Initialize()
 {
-    GetNativeSystemInfo(&m_systemInfo);
-    m_dwMaxAddress = (DWORD64)m_systemInfo.lpMaximumApplicationAddress - (DWORD64)m_systemInfo.lpMinimumApplicationAddress;
+   
 }
 
 void CGuardBase::StaticPulse(void* pContext)
--- FivemEngine/Guards/CGuardBase.h
+++ FivemEngine/Guards/CGuardBase.h
@@ -5,10 +5,4 @@ public:
     virtual void Initialize();
     static void  StaticPulse(void* pContext);
     virtual void DoPulse();
-
-protected:
-    MEMORY_BASIC_INFORMATION m_mbi;
-    SYSTEM_INFO              m_systemInfo;
-    DWORD64                  m_dwMaxAddress;
-    DWORD64                  m_dwCurrentAddress;
 };
