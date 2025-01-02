21f128e04549241b22b7dc177b061304dceb9f6c
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-01-02 22:22:38 +0100

    Removed some old functions/members in the CHeuristicGuard class

--- AtomicEngine/Guards/CHeuristicGuard.h
+++ AtomicEngine/Guards/CHeuristicGuard.h
@@ -16,10 +16,6 @@ public:
     static void StaticPulse(void* pContext) { reinterpret_cast<CHeuristicGuard*>(pContext)->DoPulse(); }
     void        DoPulse() override {}
 
-    unsigned int GetLatestScanResult() { return m_uiLatestScanResult; }
-    void         UpdateLatestScanResult(unsigned int uiScanResult) { m_uiLatestScanResult = uiScanResult; }
-
 private:
-    unsigned int                                    m_uiLatestScanResult;
     //std::map<std::string, std::unordered_set<std::string>> m_Signatures;
 };
