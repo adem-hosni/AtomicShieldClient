cc781060b1765694021551166017f1ab350205ba
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-01-06 20:46:29 +0100

    Update decryption logic

--- AtomicAgent/CAtomicCore.cpp
+++ AtomicAgent/CAtomicCore.cpp
@@ -10,10 +10,37 @@ CAtomicCore* g_pAtomicCore = new CAtomicCore();
 std::string PadBuffer(const std::string& buffer)
 {
     constexpr size_t blockSize = 16;
-    size_t paddingSize = blockSize - (buffer.size() % blockSize);
-    std::string paddedBuffer = buffer;
+    size_t           paddingSize = blockSize - (buffer.size() % blockSize);
+    std::string      paddedBuffer = buffer;
     paddedBuffer.append(paddingSize, static_cast<char>(paddingSize));
     return paddedBuffer;
+}
+
+std::string UnpadBuffer(std::string& input)
+{
+    if (input.empty())
+        return "";
+
+    // The padding length is stored in the last byte of the string (character).
+    size_t pad_len = static_cast<size_t>(input.back());
+    if (pad_len == 0 || pad_len > 16)
+    {
+        SharedUtil::AddDebugLog("Invalid padding length");
+        return "";
+    }
+
+    // Check if the padding is valid
+    for (size_t i = 0; i < pad_len; ++i)
+    {
+        if (input[input.size() - 1 - i] != static_cast<char>(pad_len))
+        {
+            SharedUtil::AddDebugLog("Invalid padding!");
+            return "";
+        }
+    }
+
+    // Create a new string without the padding
+    return input.substr(0, input.size() - pad_len);
 }
 
 CAtomicCore::CAtomicCore()
@@ -32,14 +59,14 @@ std::string CAtomicCore::Encrypt(std::string buffer)
 
     buffer = PadBuffer(buffer);
 
-    const int& byte_key_index = SharedUtil::GenerateRandomNumber(0, m_vAESKeys.size() - 1);
+    const int&        byte_key_index = SharedUtil::GenerateRandomNumber(0, m_vAESKeys.size() - 1);
     std::vector<BYTE> key = m_vAESKeys[byte_key_index];
     std::vector<BYTE> iv = m_vAESIVs[byte_key_index];
 
     AES aes(AESKeyLength::AES_256);
 
     std::vector<BYTE> plaintext(buffer.begin(), buffer.end());
-    std::vector<BYTE>             out = aes.EncryptCBC(plaintext, key, iv);
+    std::vector<BYTE> out = aes.EncryptCBC(plaintext, key, iv);
 
     std::string outbuffer(out.begin(), out.end());
     outbuffer = (char)(byte_key_index + 31) + outbuffer;
@@ -59,64 +86,13 @@ std::string CAtomicCore::Decrypt(std::string strbuffer)
     const auto iv = m_vAESIVs[ikey_index];
 
     // Extract encrypted buffer
-    std::string encrypted_buffer = strbuffer.substr(1);            // Skip first byte
+    std::string buffer = strbuffer.substr(1);            // Skip first byte
 
-    BCRYPT_ALG_HANDLE hAlgorithm;
-    BCRYPT_KEY_HANDLE hKey;
-    NTSTATUS          status;
-    DWORD             keyObjectSize = 0, resultSize = 0, plaintextSize = 0;
+    AES aes(AESKeyLength::AES_256);
 
-    // Open AES algorithm provider
-    status = BCryptOpenAlgorithmProvider(&hAlgorithm, BCRYPT_AES_ALGORITHM, NULL, 0);
-    if (!BCRYPT_SUCCESS(status))
-    {
-        SharedUtil::AddDebugLog("Failed to open AES algorithm provider", status);
-    }
+    std::vector<BYTE> plaintext(buffer.begin(), buffer.end());
+    std::vector<BYTE> out = aes.DecryptCBC(plaintext, key, iv);
 
-    // Set the encryption mode to CBC explicitly (this is the default for AES)
-    status = BCryptSetProperty(hAlgorithm, BCRYPT_CHAINING_MODE, (PBYTE)BCRYPT_CHAIN_MODE_CBC, sizeof(BCRYPT_CHAIN_MODE_CBC), 0);
-    if (!BCRYPT_SUCCESS(status))
-    {
-        BCryptCloseAlgorithmProvider(hAlgorithm, 0);
-        SharedUtil::AddDebugLog("Failed to set chaining mode to CBC.");
-    }
-
-    // Get the key object size
-    status = BCryptGetProperty(hAlgorithm, BCRYPT_OBJECT_LENGTH, (PUCHAR)&keyObjectSize, sizeof(DWORD), &resultSize, 0);
-    if (!BCRYPT_SUCCESS(status))
-    {
-        SharedUtil::AddDebugLog("Failed to get key object size", status);
-    }
-
-    std::vector<BYTE> keyObject(keyObjectSize);
-
-    // Create AES key
-    status = BCryptGenerateSymmetricKey(hAlgorithm, &hKey, keyObject.data(), keyObjectSize, const_cast<BYTE*>(key.data()), key.size(), 0);
-    if (!BCRYPT_SUCCESS(status))
-    {
-        SharedUtil::AddDebugLog("Failed to generate AES key", status);
-    }
-
-    // Calculate the plaintext size
-    status = BCryptDecrypt(hKey, reinterpret_cast<BYTE*>(encrypted_buffer.data()), encrypted_buffer.size(), NULL, const_cast<BYTE*>(iv.data()), iv.size(), NULL,
-                           0, &plaintextSize, BCRYPT_BLOCK_PADDING);
-    if (!BCRYPT_SUCCESS(status))
-    {
-        SharedUtil::AddDebugLog("Failed to calculate plaintext size (0x%x)", status);
-    }
-
-    std::vector<BYTE> plaintext(plaintextSize);
-
-    // Perform decryption
-    status = BCryptDecrypt(hKey, reinterpret_cast<BYTE*>(encrypted_buffer.data()), encrypted_buffer.size(), NULL, const_cast<BYTE*>(iv.data()), iv.size(),
-                           plaintext.data(), plaintext.size(), &resultSize, BCRYPT_BLOCK_PADDING);
-    if (!BCRYPT_SUCCESS(status))
-    {
-        SharedUtil::AddDebugLog("Failed to decrypt ciphertext (0x%x)", status);
-    }
-    // Cleanup
-    BCryptDestroyKey(hKey);
-    BCryptCloseAlgorithmProvider(hAlgorithm, 0);
-    std::string decrypted_buffer = reinterpret_cast<char*>(plaintext.data());
-    return decrypted_buffer;
+    std::string outbuffer(out.begin(), out.end());
+    return UnpadBuffer(outbuffer);
 }
