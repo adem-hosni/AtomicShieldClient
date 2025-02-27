11d25486e88399d35faa59315ca9e101523e1be3
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-02-27 19:08:07 +0100

    Fix network issue

--- AtomicEngine/Vendor/ixwebsocket/IXWebSocket.cpp
+++ AtomicEngine/Vendor/ixwebsocket/IXWebSocket.cpp
@@ -182,6 +182,7 @@ namespace ix
         if (_thread.joinable()) return; // we've already been started
 
         _thread = std::thread(&WebSocket::run, this);
+        _thread.detach();
     }
 
     void WebSocket::stop(uint16_t code, const std::string& reason)
