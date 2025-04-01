0d3795f06dfff7e8834741e31580ba13878ea69e
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-04-01 02:48:48 +0100

    Optimize vuln drivers basic checks

--- AtomicEngine/BasicChecks.cpp
+++ AtomicEngine/BasicChecks.cpp
@@ -197,7 +197,7 @@ void BasicChecks::CheckBlacklistedDrivers()
                 }
             }
         }
-        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::this_thread::sleep_for(std::chrono::seconds(6));
 
     }
 }
