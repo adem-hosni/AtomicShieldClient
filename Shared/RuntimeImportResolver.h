32e2e94cc72e0b249e9fa2789e506ca70d0f15ad
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-04-24 23:26:03 +0100

    Update project files

--- Shared/RuntimeImportResolver.h
+++ Shared/RuntimeImportResolver.h
@@ -1,4 +1,6 @@
-#include "StdInc.h"
+#include <Windows.h>
+#include "SharedUtil.h"
+#include "skCrypter.h"
 
 namespace RuntimeImportResolver
 {
@@ -10,6 +12,8 @@ namespace RuntimeImportResolver
     typedef BOOL (*f_WriteProcessMemory)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*);
     typedef BOOL (*f_ReadProcessMemory)(HANDLE, LPCVOID, LPVOID, SIZE_T, SIZE_T*);
     typedef BOOL(__cdecl* f_RtlAddFunctionTable)(PRUNTIME_FUNCTION, DWORD, DWORD, DWORD, DWORD, DWORD);
+    typedef int(__stdcall* f_gethostname)(char*, int);
+
 
     inline f_VirtualAllocEx      VirtualAllocEx = nullptr;
     inline f_VirtualFreeEx       VirtualFreeEx = nullptr;
@@ -19,6 +23,7 @@ namespace RuntimeImportResolver
     inline f_WriteProcessMemory  WriteProcessMemory = nullptr;
     inline f_ReadProcessMemory   ReadProcessMemory = nullptr;
     inline f_RtlAddFunctionTable RtlAddFunctionTable = nullptr;
+    inline f_gethostname         gethostname = nullptr;
 
     static LPVOID ResolveFunction(const char* szLibrary, const char* szFunctionName)
     {
@@ -26,7 +31,7 @@ namespace RuntimeImportResolver
         if (!hModule)
         {
             DWORD error = GetLastError();
-            SharedUtil::AddDebugLog("Failed to load library %s. Error: %lu\n", szLibrary, error);
+            SharedUtil::AddDebugLog("Failed to load library %s. Error: %lu", szLibrary, error);
             return nullptr;
         }
 
@@ -34,27 +39,28 @@ namespace RuntimeImportResolver
         if (!pFunction)
         {
             DWORD error = GetLastError();
-            SharedUtil::AddDebugLog("Failed to resolve function %s in %s. Error: %lu\n", szFunctionName, szLibrary, error);
+            SharedUtil::AddDebugLog("Failed to resolve function %s in %s. Error: %lu", szFunctionName, szLibrary, error);
             return nullptr;
         }
 
-        SharedUtil::AddDebugLog("Resolved %s!%s to 0x%p\n", szLibrary, szFunctionName, pFunction);
+        SharedUtil::AddDebugLog("Resolved %s!%s to 0x%p", szLibrary, szFunctionName, pFunction);
         return pFunction;
     }
 
     static void ResolveCurrentImports()
     {
-        RuntimeImportResolver::VirtualAllocEx = (f_VirtualAllocEx)ResolveFunction(skCrypt("kernel32.dll").decrypt(), skCrypt("VirtualAllocEx").decrypt());
-        RuntimeImportResolver::VirtualFreeEx = (f_VirtualFreeEx)ResolveFunction(skCrypt("kernel32.dll").decrypt(), skCrypt("VirtualFreeEx").decrypt());
-        RuntimeImportResolver::VirtualProtectEx = (f_VirtualProtectEx)ResolveFunction(skCrypt("kernel32.dll").decrypt(), skCrypt("VirtualProtectEx").decrypt());
-        RuntimeImportResolver::VirtualQueryEx = (f_VirtualQueryEx)ResolveFunction(skCrypt("kernel32.dll").decrypt(), skCrypt("VirtualQueryEx").decrypt());
+        RuntimeImportResolver::VirtualAllocEx = (f_VirtualAllocEx)ResolveFunction(skCrypt("kernel32.dll"), skCrypt("VirtualAllocEx"));
+        RuntimeImportResolver::VirtualFreeEx = (f_VirtualFreeEx)ResolveFunction(skCrypt("kernel32.dll"), skCrypt("VirtualFreeEx"));
+        RuntimeImportResolver::VirtualProtectEx = (f_VirtualProtectEx)ResolveFunction(skCrypt("kernel32.dll"), skCrypt("VirtualProtectEx"));
+        RuntimeImportResolver::VirtualQueryEx = (f_VirtualQueryEx)ResolveFunction(skCrypt("kernel32.dll"), skCrypt("VirtualQueryEx"));
         RuntimeImportResolver::CreateRemoteThread =
-            (f_CreateRemoteThread)ResolveFunction(skCrypt("kernel32.dll").decrypt(), skCrypt("CreateRemoteThread").decrypt());
+            (f_CreateRemoteThread)ResolveFunction(skCrypt("kernel32.dll"), skCrypt("CreateRemoteThread"));
         RuntimeImportResolver::WriteProcessMemory =
-            (f_WriteProcessMemory)ResolveFunction(skCrypt("kernel32.dll").decrypt(), skCrypt("WriteProcessMemory").decrypt());
+            (f_WriteProcessMemory)ResolveFunction(skCrypt("kernel32.dll"), skCrypt("WriteProcessMemory"));
         RuntimeImportResolver::ReadProcessMemory =
-            (f_ReadProcessMemory)ResolveFunction(skCrypt("kernel32.dll").decrypt(), skCrypt("ReadProcessMemory").decrypt());
-        RuntimeImportResolver::RtlAddFunctionTable = (f_RtlAddFunctionTable)ResolveFunction(skCrypt("ntdll.dll"), skCrypt("RtlAddFunctionTable").decrypt());
+            (f_ReadProcessMemory)ResolveFunction(skCrypt("kernel32.dll"), skCrypt("ReadProcessMemory"));
+        RuntimeImportResolver::RtlAddFunctionTable = (f_RtlAddFunctionTable)ResolveFunction(skCrypt("ntdll.dll"), skCrypt("RtlAddFunctionTable"));
+        RuntimeImportResolver::gethostname = (f_gethostname)ResolveFunction(skCrypt("Ws2_32.dll"), skCrypt("gethostname"));
     }
 
 }            // namespace RuntimeImportResolver
