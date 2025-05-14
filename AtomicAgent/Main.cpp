6377341b064752893bab1d10c499acad91d60839
GanjaXTn69 <136641689+GanjaXTn69@users.noreply.github.com>
2025-05-14 23:05:11 +0100

    comment shits to run fast

--- AtomicAgent/Main.cpp
+++ AtomicAgent/Main.cpp
@@ -22,28 +22,27 @@ void ApiChecks(LPVOID lpThreadParameter)
         if (result->Status.contains("message"))
             result->strMessage = result->Status["message"].as<std::string>();
     }
-    else
-    {
-        if (g_pAtomicAPI->IsValidVersion(PROJECT_VERSION))
-        {
-            if (g_pAtomicAPI->IsAlreadyConnected())
-            {
-                result->bSuccess = false;
-                result->strTitle = skCrypt("ALREADY CONNECTED");
-                result->strMessage = skCrypt("You are already connected to the network.");
-            }
-        }
-        else
-        {
-            result->bSuccess = false;
-            result->strTitle = skCrypt("OUTDATED VERSION");
-            result->strMessage = skCrypt("This version of AtomicShield is no longer supported. Please update to the latest version to continue.");
-        }
-    }
+    //else
+    //{
+    //    if (g_pAtomicAPI->IsValidVersion(PROJECT_VERSION))
+    //    {
+    //        if (g_pAtomicAPI->IsAlreadyConnected())
+    //        {
+    //            result->bSuccess = false;
+    //            result->strTitle = skCrypt("ALREADY CONNECTED");
+    //            result->strMessage = skCrypt("You are already connected to the network.");
+    //        }
+    //    }
+    //    else
+    //    {
+    //        result->bSuccess = false;
+    //        result->strTitle = skCrypt("OUTDATED VERSION");
+    //        result->strMessage = skCrypt("This version of AtomicShield is no longer supported. Please update to the latest version to continue.");
+    //    }
+    //}
     result->bInitialized = true;
 }
 
-// int main(int argc, char* argv[])
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
 {
 #ifdef _DEBUG
@@ -60,14 +59,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
     //  SharedProtocols::CheckLauncherProcess();
 
         INT CPUInfo[4] = {-1};
-
-    //__cpuid(CPUInfo, 1);
-
     if ((CPUInfo[2] >> 31) & 1 || StaticAnalysisBypass::IsAnalysisVM())
     {
         while (true)
         {
-            // Perform some math operations to keep the CPU busy like a legit process hahahaha
             5 + 8;
             8 + 9;
             Sleep(1000);
