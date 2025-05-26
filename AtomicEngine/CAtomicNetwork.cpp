968b5d0a811b0d8f8d8342127fac3edc76fd3149
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-05-27 00:30:32 +0100

    Update WebSocket ping interval
    Changed the WebSocket ping interval from 20 to 45 seconds in CAtomicNetwork.cpp to adjust the connection keep-alive behavior.

--- AtomicEngine/CAtomicNetwork.cpp
+++ AtomicEngine/CAtomicNetwork.cpp
@@ -22,7 +22,7 @@ bool CAtomicNetwork::Connect()
     m_pWebSocket->setUrl(WEBSOCKET_BASE_URL "/c/atomicshieldagent/");
     
     m_pWebSocket->setOnMessageCallback(std::bind(&CAtomicNetwork::OnReceivePacket, this, std::placeholders::_1));
-    m_pWebSocket->setPingInterval(20);
+    m_pWebSocket->setPingInterval(45);
     m_pWebSocket->enablePerMessageDeflate();
     m_pWebSocket->enablePong();
     m_pWebSocket->disableAutomaticReconnection();
