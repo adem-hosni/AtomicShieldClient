3927c3470323f9e80de9564e5e5b20405adb7f70
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-06-05 14:56:41 +0100

    Revert "Refactor memory section name struct in KernelCalls"
    This reverts commit 1239579402fb875079960729c0dba5204816970b.

--- AtomicEngine/Guards/CHeuristicGuard.cpp
+++ AtomicEngine/Guards/CHeuristicGuard.cpp
@@ -142,7 +142,7 @@ void CHeuristicGuard::DoPulse()
                 continue;
 
             NTSTATUS            status;
-            KernelCalls_MEMORY_SECTION_NAME sectionName;
+            MEMORY_SECTION_NAME sectionName;
             SIZE_T              returnLength0;
             WCHAR               filenameBuffer[MAX_PATH];
 
