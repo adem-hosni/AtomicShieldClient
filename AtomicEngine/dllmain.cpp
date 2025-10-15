255b670593c5af3e96ee421e592130e899892377
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-10-15 13:41:49 +0100

    Update dllmain.cpp

--- AtomicEngine/dllmain.cpp
+++ AtomicEngine/dllmain.cpp
@@ -88,12 +88,7 @@ void EntryPoint(LPVOID lpAntiCheatModuleBase)
 
     // SharedProtocols::EnableProcessMitigations();
 
-    CLatencyEvaluator::SetupServerEndPoint(
-        [](std::string strBestEndPoint) -> void
-        {
-            strBestEndPoint += ":8002";
-            g_pAtomicAntiCheat->GetNetwork()->SetServerEndPoint(strBestEndPoint);
-        },
+    CLatencyEvaluator::SetupServerEndPoint([](std::string strBestEndPoint) -> void { g_pAtomicAntiCheat->GetNetwork()->SetServerEndPoint(strBestEndPoint); },
                                            false);
 
     EnableDebugPrivilege();
