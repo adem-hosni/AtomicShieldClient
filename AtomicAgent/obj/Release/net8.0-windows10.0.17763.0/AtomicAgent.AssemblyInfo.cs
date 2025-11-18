b749a7371a4c3f222a678d2368326e64a5af6b10
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2025-11-18 16:53:57 +0100

    Remove obj files

--- AtomicAgent/obj/Release/net8.0-windows10.0.17763.0/AtomicAgent.AssemblyInfo.cs
+++ AtomicAgent/obj/Release/net8.0-windows10.0.17763.0/AtomicAgent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atomic Shield")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa54efd693bf3fd3f3fba699b40c13ae86c0a8ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d7257324e72930258f73510692666d5d985c08")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtomicAgent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtomicAgent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
